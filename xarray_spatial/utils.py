7f4b2a795b0d76ced0ec9033f00b9dbdf657eaf9
brendancol <bcollins@parietal.io>
2020-02-09 00:30:11 -0600

    added utils file

---
+++ xarray_spatial/utils.py
@@ -0,0 +1,31 @@
+import numba as nb
+
+ngjit = nb.jit(nopython=True, nogil=True)
+
+
+def lnglat_to_meters(longitude, latitude):
+    """
+    Projects the given (longitude, latitude) values into Web Mercator
+    coordinates (meters East of Greenwich and meters North of the Equator).
+
+    Longitude and latitude can be provided as scalars, Pandas columns,
+    or Numpy arrays, and will be returned in the same form.  Lists
+    or tuples will be converted to Numpy arrays.
+
+    Examples:
+       easting, northing = lnglat_to_meters(-40.71,74)
+
+       easting, northing = lnglat_to_meters(np.array([-74]),np.array([40.71]))
+
+       df=pandas.DataFrame(dict(longitude=np.array([-74]),latitude=np.array([40.71])))
+       df.loc[:, 'longitude'], df.loc[:, 'latitude'] = lnglat_to_meters(df.longitude,df.latitude)
+    """
+    if isinstance(longitude, (list, tuple)):
+        longitude = np.array(longitude)
+    if isinstance(latitude, (list, tuple)):
+        latitude = np.array(latitude)
+
+    origin_shift = np.pi * 6378137
+    easting = longitude * origin_shift / 180.0
+    northing = np.log(np.tan((90 + latitude) * np.pi / 360.0)) * origin_shift / np.pi
+    return (easting, northing)
