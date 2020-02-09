5cce6acf2998effe172e4ae9ee1234fd744ddc7a
brendancol <bcollins@parietal.io>
2020-02-09 01:19:09 -0600

    stubbed out bump and perlin

---
+++ xarray_spatial/bump.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+from xarray import DataArray
+
+from xarray_spatial.utils import ngjit
+
+
+# TODO: change parameters to take agg instead of height / width
+def bump(width, height, count=None, height_func=None, spread=1):
+    """
+    Generate a simple bump map
+
+    Parameters
+    ----------
+    width : int
+    height : int
+    count : int (defaults: w * h / 10)
+    height_func : function which takes x, y and returns a height value
+    spread : tuple boundaries
+
+    Returns
+    -------
+    bumpmap: DataArray
+
+    Notes:
+    ------
+    Algorithm References:
+     - http://www.mountaincartography.org/mt_hood/pdfs/nighbert_bump1.pdf
+    """
+
+    linx = range(width)
+    liny = range(height)
+
+    if count is None:
+        count = width * height // 10
+
+    if height_func is None:
+        height_func = lambda bumps: np.ones(len(bumps))
+
+    # create 2d array of random x, y for bump locations
+    locs = np.empty((count, 2), dtype=np.uint16)
+    locs[:, 0] = np.random.choice(linx, count)
+    locs[:, 1] = np.random.choice(liny, count)
+
+    heights = height_func(locs)
+
+    bumps = _finish_bump(width, height, locs, heights, spread)
+    return DataArray(bumps, dims=['y', 'x'], attrs=dict(res=1))
+
+
+@ngjit
+def _finish_bump(width, height, locs, heights, spread):
+    out = np.zeros((height, width))
+    rows, cols = out.shape
+    s = spread ** 2  # removed sqrt for perf.
+    for i in range(len(heights)):
+        x = locs[i][0]
+        y = locs[i][1]
+        z = heights[i]
+        out[y, x] = out[y, x] + z
+        if s > 0:
+            for nx in range(max(x - spread, 0), min(x + spread, width)):
+                for ny in range(max(y - spread, 0), min(y + spread, height)):
+                    d2 = (nx - x) * (nx - x) + (ny -  y) * (ny - y)
+                    if d2 <= s:
+                        out[ny, nx] = out[ny,nx] + (out[y, x] * (d2 / s))
+    return out
---
+++ xarray_spatial/perlin.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+from xarray import DataArray
+
+from xarray_spatial.utils import ngjit
+
+
+# TODO: change parameters to take agg instead of height / width
+def perlin(width, height, freq=(1, 1), seed=5):
+    """
+    Generate perlin noise aggregate
+
+    Parameters
+    ----------
+    width : int
+    height : int
+    freq : tuple of (x, y) frequency multipliers
+    seed : int
+
+    Returns
+    -------
+    bumpmap: DataArray
+
+    Notes:
+    ------
+    Algorithm References:
+    - numba-ized from Paul Panzer example available here:
+    https://stackoverflow.com/questions/42147776/producing-2d-perlin-noise-with-numpy
+     - http://www.mountaincartography.org/mt_hood/pdfs/nighbert_bump1.pdf
+    """
+    linx = range(width)
+    liny = range(height)
+    linx = np.linspace(0, 1, width, endpoint=False)
+    liny = np.linspace(0, 1, height, endpoint=False)
+    x, y = np.meshgrid(linx, liny)
+    data = _perlin(x * freq[0], y * freq[1], seed=seed)
+    data = (data - np.min(data))/np.ptp(data)
+    return DataArray(data, dims=['y', 'x'], attrs=dict(res=1))
+
+
+@ngjit
+def _lerp(a, b, x):
+    return a + x * (b-a)
+
+
+@ngjit
+def _fade(t):
+    return 6 * t**5 - 15 * t**4 + 10 * t**3
+
+
+@ngjit
+def _gradient(h, x, y):
+    vectors = np.array([[0,1],[0,-1],[1,0],[-1,0]])
+    dim_ = h.shape
+    out = np.zeros(dim_)
+    for j in range(dim_[1]):
+        for i in range(dim_[0]):
+            f = np.mod(h[i,j], 4)
+            g = vectors[f]
+            out[i,j] = g[0] * x[i,j] + g[1] * y[i,j]
+    return out
+
+
+def _perlin(x, y, seed=0):
+    np.random.seed(seed)
+    p = np.arange(2**20,dtype=int)
+    np.random.shuffle(p)
+    p = np.stack([p,p]).flatten()
+
+    # coordinates of the top-left
+    xi = x.astype(int)
+    yi = y.astype(int)
+
+    # internal coordinates
+    xf = x - xi
+    yf = y - yi
+
+    # fade factors
+    u = _fade(xf)
+    v = _fade(yf)
+
+    # noise components
+    n00 = _gradient(p[p[xi]+yi], xf, yf)
+    n01 = _gradient(p[p[xi]+yi+1], xf, yf-1)
+    n11 = _gradient(p[p[xi+1]+yi+1], xf-1, yf-1)
+    n10 = _gradient(p[p[xi+1]+yi], xf-1, yf)
+
+    # combine noises
+    x1 = _lerp(n00, n10, u)
+    x2 = _lerp(n01, n11, u)
+    a = _lerp(x1, x2, v)
+    return a
