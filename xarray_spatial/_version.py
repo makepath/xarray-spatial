b950f7774de240d0778095e728610ebe59173212
brendancol <bcollins@parietal.io>
2020-02-09 21:44:58 -0600

    versioneer update

--- xarray_spatial/_version.py
+++ xarray_spatial/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
-    cfg.versionfile_source = "tile_fetch/_version.py"
+    cfg.versionfile_source = "xarray_spatial/_version.py"
     cfg.verbose = False
     return cfg
 
