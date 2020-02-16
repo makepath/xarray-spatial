8d31c7bdc7f45fdad0ca04c69a4132702b4feff8
brendancol <bcollins@parietal.io>
2020-02-15 19:03:50 -0600

    added version number

--- xrspatial/__init__.py
+++ xrspatial/__init__.py
@@ -11,3 +11,18 @@ from xrspatial.ndvi import ndvi  # noqa
 from xrspatial.zonal import stats as zonal_stats  # noqa
 from xrspatial.zonal import apply as zonal_apply  # noqa
 from xrspatial.zonal import crosstab as zonal_crosstab  # noqa
+
+
+__version__ = '0.0.1'
+
+
+def test():
+    """Run the xarray-spatial test suite."""
+    import os
+    try:
+        import pytest
+    except ImportError:
+        import sys
+        sys.stderr.write("You need to install py.test to run tests.\n\n")
+        raise
+    pytest.main([os.path.dirname(__file__)])
