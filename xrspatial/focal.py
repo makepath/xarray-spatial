de957296d853223e49e35f3cf87718bcd3cec928
thuydotm <mdo@DESKTOP-M6T973D.localdomain>
2020-04-05 15:07:48 -0400

    handle distance unit

--- xrspatial/focal.py
+++ xrspatial/focal.py
@@ -2,9 +2,11 @@ import numpy as np
 from xarray import DataArray
 from xrspatial.utils import ngjit
 from numba import stencil
+import re
 
+DEFAULT_UNIT = 'meter'
 
-#TODO: Make convolution more generic with numba first-class functions.
+# TODO: Make convolution more generic with numba first-class functions.
 
 
 @ngjit
@@ -53,6 +55,104 @@ def mean(agg, passes=1, excludes=[np.nan]):
 
     return DataArray(out, name='mean',
                      dims=agg.dims, coords=agg.coords, attrs=agg.attrs)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+# modified from https://stackoverflow.com/questions/3943752/the-dateutil-parser-parse-of-distance-strings
+class Distance(object):
+    METER = 1
+    FOOT = 0.3048
+    KILOMETER = 1000
+    MILE = 1609.344
+    UNITS = {'meter': METER,
+             'meters': METER,
+             'm': METER,
+             'feet': FOOT,
+             'foot': FOOT,
+             'ft': FOOT,
+             'miles': MILE,
+             'mls': MILE,
+             'ml': MILE,
+             'kilometer': KILOMETER,
+             'kilometers': KILOMETER,
+             'km': KILOMETER,
+             }
+
+    def __init__(self, s):
+        self.number, unit = self._get_distance_unit(s)
+        self._convert(unit)
+
+    def _get_distance_unit(self, s):
+        # spit string into numbers and text
+        splits = [x for x in re.split(r'(-?\d*\.?\d+)', s) if x != '']
+
+        if len(splits) not in [1, 2]:
+            raise ValueError("Invalid distance.")
+
+        number = splits[0]
+        unit = DEFAULT_UNIT
+        if len(splits) == 1:
+            print("Distance unit not provided. Use meter as default.")
+        elif len(splits) == 2:
+            unit = splits[1]
+
+        unit = unit.lower()
+        if unit not in self.UNITS:
+            raise ValueError(
+                "Invalid value.\n"
+                "Distance unit should be one of the following: \n"
+                "meter (meter, meters, m),\n"
+                "kilometer (kilometer, kilometers, km),\n"
+                "foot (foot, feet, ft),\n"
+                "mile (mile, miles, ml, mls)")
+        return number, unit
+
+    def _convert(self, unit):
+        self.number = float(self.number)
+        if self.UNITS[unit] != 1:
+            self.number *= self.UNITS[unit]
+
+    @property
+    def meters(self):
+        return self.number
+
+    @meters.setter
+    def meters(self, v):
+        self.number = float(v)
+
+    @property
+    def miles(self):
+        return self.number / self.MILE
+
+    @miles.setter
+    def miles(self, v):
+        self.number = v
+        self._convert('miles')
+
+    @property
+    def feet(self):
+        return self.number / self.FOOT
+
+    @feet.setter
+    def feet(self, v):
+        self.number = v
+        self._convert('feet')
+
+    @property
+    def kilometers(self):
+        return self.number / self.KILOMETER
+
+    @kilometers.setter
+    def kilometers(self, v):
+        self.number = v
+        self._convert('KILOMETER')
 
 
 def _zscores(array):
@@ -128,12 +228,21 @@ def focal_analysis(raster, shape='circle', radius=1):
                 cell_size_y = raster[dim].values[1] - raster[dim].values[0]
 
     # TODO: check coordinate unit, convert from lat-lon to meters
+    if 'unit' in raster.attrs:
+        unit = raster.attrs['unit']
+    else:
+        unit = DEFAULT_UNIT
+        print("Raster distance unit not provided. Use meter as default.")
+
+    sx = Distance(str(cell_size_x) + unit)
+    sy = Distance(str(cell_size_y) + unit)
+    sr = Distance(str(radius))
 
     # create kernel
     if shape == 'circle':
         # convert radius (meter) to pixel
-        kernel_half_w = int(radius / cell_size_x)
-        kernel_half_h = int(radius / cell_size_y)
+        kernel_half_w = int(sr.meters / sx.meters)
+        kernel_half_h = int(sr.meters / sy.meters)
         kernel = _gen_ellipse_kernel(kernel_half_w, kernel_half_h)
 
     # zero padding
