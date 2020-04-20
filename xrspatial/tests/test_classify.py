d2c97bda90a4b5efd56aecf7ae86c48716aa68da
brendancol <bcollins@parietal.io>
2020-04-19 22:54:05 -0500

    added classify module with quantile func

---
+++ xrspatial/tests/test_classify.py
@@ -0,0 +1,23 @@
+import xarray as xr
+import numpy as np
+
+from xrspatial.classify import quantile
+
+
+def test_quantile():
+    k = 5
+    n, m = 5, 5
+    agg = xr.DataArray(np.arange(n*m).reshape((n, m)), dims=['x', 'y'])
+    agg['x'] = np.linspace(0, n, n)
+    agg['y'] = np.linspace(0, m, m)
+
+    quantile_agg = quantile(agg, k=5)
+    assert quantile_agg is not None
+
+    print(quantile_agg)
+    print(quantile_agg.mean())
+
+    unique_elements, counts_elements = np.unique(quantile_agg.data,
+                                                 return_counts=True)
+    assert len(unique_elements) == k
+    assert len(counts_elements) == 1
