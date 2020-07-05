61ffeb8ea467bee1fd56d6dd5012f5ac87acf4a9
Brendan Collins <brendancol@gmail.com>
2020-07-05 18:06:07 -0500

    Spectral tools (#64)
    * renamed ndvi to spectral
    
    * wip adding additional multispectral tools #19
    
    * normalize_data dim swap
    
    * added multispectral tests
    
    * fixed setup file fixes #19

--- xrspatial/__init__.py
+++ xrspatial/__init__.py
@@ -3,18 +3,29 @@ from xrspatial.bump import bump  # noqa
 from xrspatial.curvature import curvature  # noqa
 from xrspatial.focal import mean  # noqa
 from xrspatial.hillshade import hillshade  # noqa
-from xrspatial.ndvi import ndvi  # noqa
+
+from xrspatial.multispectral import ndvi  # noqa
+from xrspatial.multispectral import arvi  # noqa
+from xrspatial.multispectral import savi  # noqa
+from xrspatial.multispectral import nbr  # noqa
+from xrspatial.multispectral import evi  # noqa
+from xrspatial.multispectral import sipi  # noqa
+
 from xrspatial.perlin import perlin  # noqa
+
 from xrspatial.proximity import euclidean_distance  # noqa
 from xrspatial.proximity import great_circle_distance  # noqa
 from xrspatial.proximity import manhattan_distance  # noqa
 from xrspatial.proximity import proximity  # noqa
+
 from xrspatial.slope import slope  # noqa
 from xrspatial.terrain import generate_terrain  # noqa
 from xrspatial.viewshed import viewshed  # noqa
+
 from xrspatial.zonal import apply as zonal_apply  # noqa
 from xrspatial.zonal import crosstab as zonal_crosstab  # noqa
 from xrspatial.zonal import stats as zonal_stats  # noqa
+
 from xrspatial.classify import quantile  # noqa
 from xrspatial.classify import equal_interval  # noqa
 from xrspatial.classify import natural_breaks  # noqa
