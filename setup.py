887e63946ecce65a147380c1661cfa7cd741cb3c
brendancol <bcollins@parietal.io>
2020-07-04 21:36:30 -0500

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 import versioneer
 
@@ -6,12 +6,12 @@ setup(name='xarray-spatial',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description='xarray-based spatial analysis tools',
-      packages=find_packages(),
+      packages=['xrspatial',
+                'xrspatial.zonal',
+                'xrspatial.tests'],
       install_requires=['datashader', 'pytest'],
       zip_safe=False,
-      classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-      ],
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
       include_package_data=True)
