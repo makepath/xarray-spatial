69ab5cb9c5ec8cf5ea7f45b6d85403035ae2b8be
thuydotm <47617499+thuydotm@users.noreply.github.com>
2020-08-10 14:28:37 -0400

    A*: an example of Austin road network (#118)
    * a star: add example of Austin road network
    
    * rename file
    
    * a star: return a xarray.DataArray
    
    * a star: float dtype for output DataArray
    
    * a star: add snap option
    
    * a star: update tests
    
    * a star: update example

--- xrspatial/tests/test_pathfinding.py
+++ xrspatial/tests/test_pathfinding.py
@@ -5,19 +5,18 @@ from xrspatial import a_star_search
 
 
 def test_a_star_search():
-    agg = xr.DataArray(np.array([[0, 1, 0, 0],
-                                 [1, 0, 0, 0],
+    agg = xr.DataArray(np.array([[1, 1, 0, 0],
+                                 [1, 1, 0, 0],
                                  [0, 1, 2, 2],
                                  [1, 0, 2, 0],
                                  [0, 2, 2, 2]]),
                        dims=['lat', 'lon'])
 
     height, width = agg.shape
-    _lon = np.linspace(0, width-1, width)
-    _lat = np.linspace(0, height-1, height)
+    _lon = np.linspace(0, width - 1, width)
+    _lat = np.linspace(0, height - 1, height)
     agg['lon'] = _lon
     agg['lat'] = _lat
-
     barriers = []
     # no barriers, there always path from a start location to a goal location
     for x0 in _lon:
@@ -26,23 +25,34 @@ def test_a_star_search():
             for x1 in _lon:
                 for y1 in _lat:
                     goal = (x1, y1)
-                    path, cost = a_star_search(agg, start, goal, barriers,
-                                               'lon', 'lat')
-                    assert len(path) > 0 and cost >= 0
-                    if start == goal:
-                        # if start and goal are in same cell,
-                        # path is the cell itself
-                        # with 0 cost
-                        assert len(path) == 1 and cost == 0
+                    path_agg = a_star_search(agg, start, goal, barriers, 'lon',
+                                             'lat')
+                    assert isinstance(path_agg, xr.DataArray)
+                    assert type(path_agg.values[0][0]) == np.float64
+                    assert path_agg.shape == agg.shape
+                    assert path_agg.dims == agg.dims
+                    assert path_agg.attrs == agg.attrs
+                    for c in path_agg.coords:
+                        assert (path_agg[c] == agg.coords[c]).all()
+                    assert np.nanmax(path_agg) >= 0 and np.nanmin(
+                        path_agg) == 0
 
     barriers = [1]
     # set pixels with value 1 as barriers,
-    # cannot go from (0, 0) to anywhere since it is bounded by 1s
+    # cannot go from (0, 0) to anywhere since it is surrounded by 1s
     start = (0, 0)
     for x1 in _lon:
         for y1 in _lat:
             goal = (x1, y1)
             if goal != start:
-                path, cost = a_star_search(agg, start, goal, barriers,
-                                           'lon', 'lat')
-                assert len(path) == 0 and cost == -1
+                path_agg = a_star_search(agg, start, goal, barriers, 'lon',
+                                         'lat')
+                assert isinstance(path_agg, xr.DataArray)
+                assert type(path_agg.values[0][0]) == np.float64
+                assert path_agg.shape == agg.shape
+                assert path_agg.dims == agg.dims
+                assert path_agg.attrs == agg.attrs
+                for c in path_agg.coords:
+                    assert (path_agg[c] == agg.coords[c]).all()
+                # no path, all cells in path_agg are nans
+                assert np.isnan(path_agg).all()
