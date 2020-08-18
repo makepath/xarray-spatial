b385cb3a2b536dc376e132c1403ef67ae8ad3893
thuydotm <thuydtm.csk9@gmail.com>
2020-08-18 17:57:19 -0400

    add reference

--- xrspatial/pathfinding.py
+++ xrspatial/pathfinding.py
@@ -263,6 +263,7 @@ def a_star_search(surface, start, goal, barriers=[], x='x', y='y',
 
     Algorithm References:
     - https://www.redblobgames.com/pathfinding/a-star/implementation.html
+    - https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
     """
 
     if surface.ndim != 2:
