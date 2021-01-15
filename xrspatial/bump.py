ddd265c8fd2ff83d37a3f02070db6fea9d90d015
Sapir Carlo Lastimoza-Dooley <sapirdooley@users.noreply.github.com>
2021-01-15 14:09:58 -0600

    add bump documentation

--- xrspatial/bump.py
+++ xrspatial/bump.py
@@ -8,24 +8,86 @@ from xrspatial.utils import ngjit
 # TODO: change parameters to take agg instead of height / width
 def bump(width, height, count=None, height_func=None, spread=1):
     """
-    Generate a simple bump map
+Generate a simple bump map to simulate the appearance of land features. 
+Using a user-defined height function, determines at what elevation a specific bump height is acceptable.
+Bumps of number "count" are applied over the area "width" x "height".
 
-    Parameters
-    ----------
+Parameters:
+----------
     width : int
+        The total width, in pixels, of the image.
+
     height : int
+        The total height, in pixels, of the image.
+
     count : int (defaults: w * h / 10)
+        The number of bumps to generate.
+
     height_func : function which takes x, y and returns a height value
+        The function used to apply varying bump heights to different elevations.
+
     spread : tuple boundaries
 
-    Returns
-    -------
-    bumpmap: DataArray
+Returns:
+----------
+    bumpmap: xarray.DataArray
 
-    Notes:
-    ------
+Notes:
+----------
     Algorithm References:
-     - http://www.mountaincartography.org/mt_hood/pdfs/nighbert_bump1.pdf
+        - http://www.mountaincartography.org/mt_hood/pdfs/nighbert_bump1.pdf
+
+Examples:
+----------
+    Imports
+>>>     import numpy as np
+>>>     import datashader as ds
+>>>     from datashader.transfer_functions import shade
+>>>     from datashader.transfer_functions import stack
+
+    Generate Terrain
+>>>     from xrspatial import generate_terrain
+
+>>>     W = 800
+>>>     H = 600
+
+>>>     cvs = ds.Canvas(plot_width = W, plot_height = H, x_range = (-20e6, 20e6), y_range = (-20e6, 20e6))
+>>>     terrain = generate_terrain(canvas=cvs)
+
+    Create Height Function
+>>>     from functools import partial
+>>>     from xrspatial import bump
+
+>>>     def heights(locations, src, src_range, height=20):
+>>>         num_bumps = locations.shape[0]
+>>>         out = np.zeros(num_bumps, dtype=np.uint16)
+>>>         for r in range(0, num_bumps):
+>>>             loc = locations[r]
+>>>             x = loc[0]
+>>>             y = loc[1]
+>>>             val = src[y, x]
+>>>             if val >= src_range[0] and val < src_range[1]:
+>>>                 out[r] = height
+>>>        return out
+
+    Create Bump Map
+>>>     bump_count = 10000
+>>>     src = terrain.data
+>>>     bumps = bump(W, H, count = bump_count, height_func = partial(heights, src = src, src_range = (1000, 1300), height = 5))
+>>>     bumps += bump(W, H, count = bump_count//2, height_func = partial(heights, src = src, src_range = (1300, 1700), height = 20))
+>>>     print(bumps)
+
+    <xarray.DataArray (y: 600, x: 800)>
+    array([[0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           ...,
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.],
+           [0., 0., 0., ..., 0., 0., 0.]])
+    Dimensions without coordinates: y, x
+    Attributes:
+        res:      1
     """
 
     linx = range(width)
