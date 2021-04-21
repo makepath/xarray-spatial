852907ab53089999e031978f1fb4d01a311dd8e9
lex-c <chlochlo.lexa@gmail.com>
2021-04-21 11:06:52 -0400

    add pytest skip for appveyor in github

--- test_examples_cli.py
+++ test_examples_cli.py
@@ -4,13 +4,14 @@ from unittest.mock import patch
 
 
 # test_args include copy-examples, fetch-data, or examples (does both)
+@pytest.mark.skip(reason="meant only for internal use")
 def run_examples_cmds(*cli_cmds):
     """
     Run conda package cli commands to download examples and fetch data
     for notebooks.
 
     Parameters: 'copy-examples', 'fetch-data', 'examples'
-    Output: downloads examples and data to new xrspatial-examples
+    Returns: downloads examples and data to new xrspatial-examples
     directory in xarray-spatialx
     """
 
