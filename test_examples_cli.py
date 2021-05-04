42998ef82f69c210abaf2c60e74abb3cd0e8cc50
Chloe <66706246+lex-c@users.noreply.github.com>
2021-05-04 10:16:04 -0400

    Only after PR#349: Test NBs onJupyterLab (#370)
    * edit sentinel mosaic and fix pathfinding
    
    * remove unnecessary imports
    
    * test all user_guide nb's to work in jupyterlab edits for clarity and for bugs
    
    * edit 6 and 7 and pharmacy deserts and datasets.yml
    
    * edit user_guide nbs
    
    * edit focal nb
    
    * edit pharmacy deserts for functionality; change natural breaks to quantile
    
    * add austin streets in datasets
    
    * edit pharmacy deserts
    
    * mv test_examples_cli for appveyer tests ignore
    
    * Revert "mv test_examples_cli for appveyer tests ignore"
    
    This reverts commit 43be8dbad29917758a59842e410450b24e06d65c.
    
    revert
    
    * revert non-related
    
    * Revert "revert non-related"
    
    This reverts commit 88a7a428092494ff8b3852b1cf14a199003f9932.
    
    * move test_examples
    
    * add pytest skip for appveyor in github
    
    * add import pytest
    
    * flake8 test_examples
    
    * move files and edit manifest to match vcs and sdist
    
    * remove fxn call in test_cli
    
    * add pharmacy deserts and remove tiling
    
    * suppress warning
    
    * edit ms
    
    Co-authored-by: Brendan Collins <brendan@makepath.com>

--- test_examples_cli.py
+++ test_examples_cli.py
@@ -1,22 +1,21 @@
 import sys
 from xrspatial import __main__ as m
 from unittest.mock import patch
+import pytest
 
 
 # test_args include copy-examples, fetch-data, or examples (does both)
+@pytest.mark.skip(reason="meant only for internal use")
 def run_examples_cmds(*cli_cmds):
     """
     Run conda package cli commands to download examples and fetch data
     for notebooks.
 
     Parameters: 'copy-examples', 'fetch-data', 'examples'
-    Output: downloads examples and data to new xrspatial-examples
+    Returns: downloads examples and data to new xrspatial-examples
     directory in xarray-spatialx
     """
 
     for arg in cli_cmds:
         with patch.object(sys, 'argv', ['xrspatial', arg]):
             m.main()
-
-
-run_examples_cmds('examples')
