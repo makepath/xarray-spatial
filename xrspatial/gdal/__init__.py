31cd53f676ba4c0f9e937c8a87575de2872842fa
lex-c <chlochlo.lexa@gmail.com>
2021-06-04 12:21:36 -0400

    add gdal

---
+++ xrspatial/gdal/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from . import (
+        reprojection,
+        reprojection_rasterio,
+        reprojection_rio
+    )
+except ImportError as e:
+    msg = (
+        "gdal module not installed.\n"
+        "Please use pip install xarray-spatial[gdal]\n"
+        "to install this module"
+    )
+    raise ImportError(str(e) + "\n" + msg) from e
