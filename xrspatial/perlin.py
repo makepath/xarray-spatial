c3eb2092a797561cd0a07d48a5eca6e0b665a921
thuydotm <thuydtm.csk9@gmail.com>
2021-09-17 17:15:51 +0700

    refactor

--- xrspatial/perlin.py
+++ xrspatial/perlin.py
@@ -1,25 +1,209 @@
+# std lib
+from functools import partial
+from typing import Union
+
+# 3rd-party
 import numpy as np
-
 import xarray as xr
-from xarray import DataArray
 
-from xrspatial.utils import ngjit
+try:
+    import cupy
+except ImportError:
+    class cupy(object):
+        ndarray = False
+
+import dask.array as da
+from numba import cuda, jit
+import numba as nb
+
+# local modules
+from xrspatial.utils import has_cuda
 
 
-# TODO: change parameters to take agg instead of height / width
-def perlin(width: int,
-           height: int,
+@jit(nopython=True, nogil=True, parallel=True, cache=True)
+def _lerp(a, b, x):
+    return a + x * (b - a)
+
+
+@jit(nopython=True, nogil=True, parallel=True, cache=True)
+def _fade(t):
+    return 6 * t ** 5 - 15 * t ** 4 + 10 * t ** 3
+
+
+@jit(nopython=True, nogil=True, parallel=True, cache=True)
+def _gradient(h, x, y):
+    # assert(len(h.shape) == 2)
+    vectors = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
+    out = np.zeros(h.shape)
+    for j in nb.prange(h.shape[1]):
+        for i in nb.prange(h.shape[0]):
+            f = np.mod(h[i, j], 4)
+            g = vectors[f]
+            out[i, j] = g[0] * x[i, j] + g[1] * y[i, j]
+    return out
+
+
+def _perlin(p, x, y):
+    # coordinates of the top-left
+    xi = x.astype(int)
+    yi = y.astype(int)
+
+    # internal coordinates
+    xf = x - xi
+    yf = y - yi
+
+    # fade factors
+    u = _fade(xf)
+    v = _fade(yf)
+
+    # noise components
+    n00 = _gradient(p[p[xi] + yi], xf, yf)
+    n01 = _gradient(p[p[xi] + yi + 1], xf, yf - 1)
+    n11 = _gradient(p[p[xi + 1] + yi + 1], xf - 1, yf - 1)
+    n10 = _gradient(p[p[xi + 1] + yi], xf - 1, yf)
+
+    # combine noises
+    x1 = _lerp(n00, n10, u)
+    x2 = _lerp(n01, n11, u)
+    a = _lerp(x1, x2, v)
+    return a
+
+
+def _perlin_numpy(data: np.ndarray,
+                  freq: tuple,
+                  seed: int) -> np.ndarray:
+    np.random.seed(seed)
+    p = np.arange(2 ** 20, dtype=int)
+    np.random.shuffle(p)
+    p = np.append(p, p)
+
+    height, width = data.shape
+    linx = np.linspace(0, freq[0], width, endpoint=False, dtype=np.float32)
+    liny = np.linspace(0, freq[1], height, endpoint=False, dtype=np.float32)
+    x, y = np.meshgrid(linx, liny)
+
+    data[:] = _perlin(p, x, y)
+    data[:] = (data - np.min(data)) / np.ptp(data)
+    return data
+
+
+def _perlin_dask_numpy(data: da.Array,
+                       freq: tuple,
+                       seed: int) -> da.Array:
+    np.random.seed(seed)
+    p = np.arange(2 ** 20, dtype=int)
+    np.random.shuffle(p)
+    p = np.append(p, p)
+
+    height, width = data.shape
+    linx = da.linspace(0, freq[0], width, endpoint=False, dtype=np.float32)
+    liny = da.linspace(0, freq[1], height, endpoint=False, dtype=np.float32)
+    x, y = da.meshgrid(linx, liny)
+
+    _func = partial(_perlin, p)
+    data = da.map_blocks(_func, x, y, meta=np.array((), dtype=np.float32))
+
+    data = (data - da.min(data)) / da.ptp(data)
+    return data
+
+
+@cuda.jit(device=True)
+def _lerp_gpu(a, b, x):
+    return a + x * (b - a)
+
+
+@cuda.jit(device=True)
+def _fade_gpu(t):
+    return 6 * t ** 5 - 15 * t ** 4 + 10 * t ** 3
+
+
+@cuda.jit(device=True)
+def _gradient_gpu(vec, h, x, y):
+    f = h % 4
+    return vec[f][0] * x + vec[f][1] * y
+
+
+@cuda.jit(fastmath=True, opt=True)
+def _perlin_gpu(p, x0, x1, y0, y1, m, out):
+    # alloc and initialize array to be used in the gradient routine
+    vec = cuda.local.array((4, 2), nb.int32)
+    vec[0][0] = vec[1][0] = vec[2][1] = vec[3][1] = 0
+    vec[0][1] = vec[2][0] = 1
+    vec[1][1] = vec[3][0] = -1
+
+    # these are the i,j coordinates of the block's first thread
+    si = cuda.blockDim.y * cuda.blockIdx.y
+    sj = cuda.blockDim.x * cuda.blockIdx.x
+
+    # while the block's elementes are still in the data's range
+    for si in range(cuda.blockDim.y * cuda.blockIdx.y, out.shape[0],
+                    cuda.gridDim.y * cuda.blockDim.y):
+        for sj in range(cuda.blockDim.x * cuda.blockIdx.x, out.shape[1],
+                        cuda.gridDim.x * cuda.blockDim.x):
+            # this the thread's element index
+            # this loop limits memory divergence
+            i = si + cuda.threadIdx.y
+            j = sj + cuda.threadIdx.x
+            if i < out.shape[0] and j < out.shape[1]:
+                # coordinates of the top-left
+                y = y0 + i * (y1 - y0) / out.shape[0]
+                x = x0 + j * (x1 - x0) / out.shape[1]
+
+                # coordinates of the top-left
+                x_int = int(x)
+                y_int = int(y)
+
+                # internal coordinates
+                xf = x - x_int
+                yf = y - y_int
+
+                # fade factors
+                u = _fade_gpu(xf)
+                v = _fade_gpu(yf)
+
+                # noise components
+                n00 = _gradient_gpu(vec, p[p[x_int] + y_int], xf, yf)
+                n01 = _gradient_gpu(vec, p[p[x_int] + y_int + 1], xf, yf - 1)
+                n11 = _gradient_gpu(vec, p[p[x_int + 1] + y_int + 1], xf - 1,
+                                    yf - 1)
+                n10 = _gradient_gpu(vec, p[p[x_int + 1] + y_int], xf - 1, yf)
+
+                # combine noises
+                x1 = _lerp_gpu(n00, n10, u)
+                x2 = _lerp_gpu(n01, n11, u)
+                out[i, j] = m * _lerp_gpu(x1, x2, v)
+
+
+def _perlin_cupy(data: cupy.ndarray,
+                 freq: tuple,
+                 seed: int) -> cupy.ndarray:
+    p = cupy.arange(2 ** 20, dtype=int)
+    cupy.random.seed(seed)
+    cupy.random.shuffle(p)
+    p = cupy.append(p, p)
+
+    blockdim = (24, 24)
+    griddim = tuple(int(d / blockdim[0] + 0.5) for d in data.shape)
+    _perlin_gpu[griddim, blockdim](p, 0, freq[0], 0, freq[1], 1, data)
+
+    minimum = cupy.amin(data)
+    maximum = cupy.amax(data)
+    data[:] = (data - minimum) / (maximum - minimum)
+    return data
+
+
+def perlin(agg: xr.DataArray,
            freq: tuple = (1, 1),
-           seed: int = 5) -> xr.DataArray:
+           seed: int = 5,
+           name: str = 'perlin') -> xr.DataArray:
     """
     Generate perlin noise aggregate.
 
     Parameters
     ----------
-    width : int
-        Width of output aggregate array.
-    height : int
-        Height of output aggregate array.
+    agg : xr.DataArray
+        2D array of size width x height, will be used to determine
+        height/ width and which platform to use for calculation.
     freq : tuple, default=(1,1)
         (x, y) frequency multipliers.
     seed : int, default=5
@@ -41,19 +225,24 @@ def perlin(width: int,
        :include-source:
 
         import matplotlib.pyplot as plt
-        from xrspatial import perlin
+        import xarray as xr
+        from xrspatial import fast_perlin
+        noise = xr.DataArray(np.zeros((H, W), dtype=np.float32),
+                        name='numpy_terrain',
+                        dims=('y', 'x'),
+                        attrs={'res': 1})
 
         # Generate Perlin Noise Aggregate
-        perlin_default = perlin(width = 500, height = 300)
+        perlin_default = fast_perlin(noise)
 
         # With Increased x Frequency
-        perlin_high_x_freq = perlin(width = 500, height = 300, freq = (5, 1))
+        perlin_high_x_freq = fast_perlin(noise, freq = (5, 1))
 
         # With Increased y Frequency
-        perlin_high_y_freq = perlin(width = 500, height = 300, freq = (1, 5))
+        perlin_high_y_freq = fast_perlin(noise, freq = (1, 5))
 
         # With a Different Seed
-        perlin_seed_1 = perlin(width = 500, height = 300, seed = 1)
+        perlin_seed_1 = fast_perlin(noise, seed = 1)
 
         # Plot Default Perlin
         perlin_default.plot(cmap = 'inferno', aspect = 2, size = 4)
@@ -109,65 +298,21 @@ def perlin(width: int,
         Attributes:
             res:      1
     """
-    linx = range(width)
-    liny = range(height)
-    linx = np.linspace(0, 1, width, endpoint=False)
-    liny = np.linspace(0, 1, height, endpoint=False)
-    x, y = np.meshgrid(linx, liny)
-    data = _perlin(x * freq[0], y * freq[1], seed=seed)
-    data = (data - np.min(data))/np.ptp(data)
-    return DataArray(data, dims=['y', 'x'], attrs=dict(res=1))
 
+    # numpy case
+    if isinstance(agg.data, np.ndarray):
+        out = _perlin_numpy(agg.data, freq, seed)
+    # cupy case
+    elif has_cuda() and isinstance(agg.data, cupy.ndarray):
+        out = _perlin_cupy(agg.data, freq, seed)
+    # dask + numpy case
+    elif isinstance(agg.data, da.Array):
+        out = _perlin_dask_numpy(agg.data, freq, seed)
+    else:
+        raise TypeError('Unsupported Array Type: {}'.format(type(agg.data)))
 
-@ngjit
-def _lerp(a, b, x):
-    return a + x * (b-a)
-
-
-@ngjit
-def _fade(t):
-    return 6 * t**5 - 15 * t**4 + 10 * t**3
-
-
-@ngjit
-def _gradient(h, x, y):
-    vectors = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
-    dim_ = h.shape
-    out = np.zeros(dim_)
-    for j in range(dim_[1]):
-        for i in range(dim_[0]):
-            f = np.mod(h[i, j], 4)
-            g = vectors[f]
-            out[i, j] = g[0] * x[i, j] + g[1] * y[i, j]
-    return out
-
-
-def _perlin(x, y, seed=0):
-    np.random.seed(seed)
-    p = np.arange(2**20, dtype=int)
-    np.random.shuffle(p)
-    p = np.stack([p, p]).flatten()
-
-    # coordinates of the top-left
-    xi = x.astype(int)
-    yi = y.astype(int)
-
-    # internal coordinates
-    xf = x - xi
-    yf = y - yi
-
-    # fade factors
-    u = _fade(xf)
-    v = _fade(yf)
-
-    # noise components
-    n00 = _gradient(p[p[xi]+yi], xf, yf)
-    n01 = _gradient(p[p[xi]+yi+1], xf, yf-1)
-    n11 = _gradient(p[p[xi+1]+yi+1], xf-1, yf-1)
-    n10 = _gradient(p[p[xi+1]+yi], xf-1, yf)
-
-    # combine noises
-    x1 = _lerp(n00, n10, u)
-    x2 = _lerp(n01, n11, u)
-    a = _lerp(x1, x2, v)
-    return a
+    result = xr.DataArray(out,
+                          dims=agg.dims,
+                          attrs=agg.attrs,
+                          name=name)
+    return result
--- xrspatial/terrain.py
+++ xrspatial/terrain.py
@@ -1,22 +1,152 @@
+# std lib
+from functools import partial
+from typing import Union, Tuple, List
+from typing import Optional
+
+# 3rd-party
 import numpy as np
+import xarray as xr
 import pandas as pd
 import datashader as ds
-from typing import Optional
-import xarray as xr
 
-from xarray import DataArray
-from .perlin import _perlin
+try:
+    import cupy
+except ImportError:
+    class cupy(object):
+        ndarray = False
+
+import dask.array as da
+
+# local modules
+from xrspatial.utils import has_cuda
+from .perlin import _perlin, _perlin_gpu
 
 
-# TODO: add optional name parameter `name='terrain'`
-def generate_terrain(x_range: tuple = (0, 500),
+def _scale(value, old_range, new_range):
+    d = (value - old_range[0]) / (old_range[1] - old_range[0])
+    return d * (new_range[1] - new_range[0]) + new_range[0]
+
+
+def _gen_terrain(height_map, seed, x_range=(0, 1), y_range=(0, 1)):
+    height, width = height_map.shape
+
+    # multiplier, (xfreq, yfreq)
+    NOISE_LAYERS = ((1 / 2**i, (2**i, 2**i)) for i in range(16))
+
+    linx = np.linspace(
+        x_range[0], x_range[1], width, endpoint=False, dtype=np.float32
+    )
+    liny = np.linspace(
+        y_range[0], y_range[1], height, endpoint=False, dtype=np.float32
+    )
+    x, y = np.meshgrid(linx, liny)
+    nrange = np.arange(2**20, dtype=int)
+    for i, (m, (xfreq, yfreq)) in enumerate(NOISE_LAYERS):
+        np.random.seed(seed+i)
+        p = np.random.permutation(nrange)
+        p = np.append(p, p)
+
+        noise = _perlin(p, x * xfreq, y * yfreq) * m
+
+        height_map += noise
+
+    height_map /= (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03)
+    height_map = height_map ** 3
+    return height_map
+
+
+def _terrain_numpy(data: np.ndarray,
+                   seed: int,
+                   x_range_scaled: tuple,
+                   y_range_scaled: tuple,
+                   zfactor: int) -> np.ndarray:
+
+    # data.fill(0)
+    data = data * 0
+    data[:] = _gen_terrain(
+        data, seed, x_range=x_range_scaled, y_range=y_range_scaled
+    )
+
+    data = (data - np.min(data))/np.ptp(data)
+    data[data < 0.3] = 0  # create water
+    data *= zfactor
+
+    return data
+
+
+def _terrain_dask_numpy(data: da.Array,
+                        seed: int,
+                        x_range_scaled: tuple,
+                        y_range_scaled: tuple,
+                        zfactor: int) -> da.Array:
+    data = data * 0
+    _func = partial(_gen_terrain, seed=seed,
+                    x_range=x_range_scaled, y_range=y_range_scaled)
+    data = da.map_blocks(_func, data, meta=np.array((), dtype=np.float32))
+
+    data = (data - da.min(data))/da.ptp(data)
+    data[data < 0.3] = 0  # create water
+    data *= zfactor
+
+    return data
+
+
+def _terrain_gpu(height_map, seed, x_range=(0, 1), y_range=(0, 1)):
+
+    NOISE_LAYERS = ((1 / 2**i, (2**i, 2**i)) for i in range(16))
+
+    noise = cupy.empty_like(height_map, dtype=np.float32)
+    nrange = cupy.arange(2**20, dtype=int)
+
+    blockdim = (24, 24)
+    griddim = (10, 80)
+    for i, (m, (xfreq, yfreq)) in enumerate(NOISE_LAYERS):
+        cupy.random.seed(seed+i)
+        p = cupy.random.permutation(nrange)
+        p = cupy.append(p, p)
+
+        _perlin_gpu[griddim, blockdim](
+            p, x_range[0] * xfreq, x_range[1] * xfreq,
+            y_range[0] * yfreq, y_range[1] * yfreq,
+            m, noise
+        )
+
+        height_map += noise
+
+    height_map /= (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03)
+    height_map = height_map ** 3
+    return height_map
+
+
+def _terrain_cupy(data: cupy.ndarray,
+                  seed: int,
+                  x_range_scaled: tuple,
+                  y_range_scaled: tuple,
+                  zfactor: int) -> cupy.ndarray:
+
+    data = data * 0
+
+    data[:] = _terrain_gpu(data, seed,
+                           x_range=x_range_scaled,
+                           y_range=y_range_scaled)
+    minimum = cupy.amin(data)
+    maximum = cupy.amax(data)
+
+    data[:] = (data - minimum)/(maximum - minimum)
+    data[data < 0.3] = 0  # create water
+    data *= zfactor
+
+    return data
+
+
+def generate_terrain(agg: xr.DataArray,
+                     x_range: tuple = (0, 500),
                      y_range: tuple = (0, 500),
-                     width: int = 25,
-                     height: int = 30,
-                     canvas: ds.Canvas = None,
                      seed: int = 10,
                      zfactor: int = 4000,
-                     full_extent: Optional[str] = None) -> xr.DataArray:
+                     full_extent: Optional[Union[Tuple, List]] = None,
+                     name: str = 'terrain'
+                     ) -> xr.DataArray:
     """
     Generates a pseudo-random terrain which can be helpful for testing
     raster functions.
@@ -27,12 +157,6 @@ def generate_terrain(x_range: tuple = (0, 500),
         Range of x values.
     x_range : tuple, default=(0, 500)
         Range of y values.
-    width : int, default=25
-        Width of output data array in pixels.
-    height : int, default=30
-        Height of output data array in pixels.
-    canvas : ds.Canvas, default=None
-        Instance for passing output dimensions / ranges.
     seed : int, default=10
         Seed for random number generator.
     zfactor : int, default=4000
@@ -56,19 +180,22 @@ def generate_terrain(x_range: tuple = (0, 500),
        :include-source:
 
         import datashader as ds
+        import xarray as xr
         import matplotlib.pyplot as plt
-        from xrspatial import generate_terrain, aspect
+        from xrspatial import generate_fast_terrain, aspect
 
         # Create Canvas
         W = 500
         H = 300
-        cvs = ds.Canvas(plot_width = W,
-                        plot_height = H,
-                        x_range = (-20e6, 20e6),
-                        y_range = (-20e6, 20e6))
+        x_range = (-20e6, 20e6)
+        y_range = (-20e6, 20e6)
+        seed = 42
+        zfactor = 4000
 
         # Generate Example Terrain
-        terrain_agg = generate_terrain(canvas = cvs)
+        data = xr.DataArray(np.zeros((H, W), dtype=np.float32),
+                            name='terrain')
+        terrain_agg = generate_fast_terrain(data, x_range, y_range, seed, zfactor)
 
         # Edit Attributes
         terrain_agg = terrain_agg.assign_attrs(
@@ -104,84 +231,53 @@ def generate_terrain(x_range: tuple = (0, 500),
             units:          km
             Max Elevation:  4000
     """
-    def _gen_heights(bumps):
-        out = np.zeros(len(bumps))
-        for i, b in enumerate(bumps):
-            x = b[0]
-            y = b[1]
-            val = agg.data[y, x]
-            if val >= 0.33 and val <= 3:
-                out[i] = 0.1
-        return out
 
-    def _scale(value, old_range, new_range):
-        d = (value - old_range[0]) / (old_range[1] - old_range[0])
-        return d * (new_range[1] - new_range[0]) + new_range[0]
+    height, width = agg.shape
 
-    mercator_extent = (-np.pi * 6378137, -np.pi * 6378137,
-                       np.pi * 6378137, np.pi * 6378137)
-    crs_extents = {'3857': mercator_extent}
-
-    if isinstance(full_extent, str):
-        full_extent = crs_extents[full_extent]
-
-    elif full_extent is None:
-        full_extent = (canvas.x_range[0], canvas.y_range[0],
-                       canvas.x_range[1], canvas.y_range[1])
+    if full_extent is None:
+        full_extent = (x_range[0], y_range[0],
+                       x_range[1], y_range[1])
 
     elif not isinstance(full_extent, (list, tuple)) and len(full_extent) != 4:
-        raise TypeError('full_extent must be tuple(4) or str wkid')
+        raise TypeError('full_extent must be tuple(4)')
 
     full_xrange = (full_extent[0], full_extent[2])
     full_yrange = (full_extent[1], full_extent[3])
 
-    x_range_scaled = (_scale(canvas.x_range[0], full_xrange, (0.0, 1.0)),
-                      _scale(canvas.x_range[1], full_xrange, (0.0, 1.0)))
+    x_range_scaled = (_scale(x_range[0], full_xrange, (0.0, 1.0)),
+                      _scale(x_range[1], full_xrange, (0.0, 1.0)))
 
-    y_range_scaled = (_scale(canvas.y_range[0], full_yrange, (0.0, 1.0)),
-                      _scale(canvas.y_range[1], full_yrange, (0.0, 1.0)))
+    y_range_scaled = (_scale(y_range[0], full_yrange, (0.0, 1.0)),
+                      _scale(y_range[1], full_yrange, (0.0, 1.0)))
 
-    data = _gen_terrain(canvas.plot_width, canvas.plot_height, seed,
-                        x_range=x_range_scaled, y_range=y_range_scaled)
+    # numpy case
+    if isinstance(agg.data, np.ndarray):
+        out = _terrain_numpy(
+            agg.data, seed, x_range_scaled, y_range_scaled, zfactor
+        )
+    # cupy case
+    elif has_cuda() and isinstance(agg.data, cupy.ndarray):
+        out = _terrain_cupy(
+            agg.data, seed, x_range_scaled, y_range_scaled, zfactor
+        )
+    # dask + numpy case
+    elif isinstance(agg.data, da.Array):
+        out = _terrain_dask_numpy(
+            agg.data, seed, x_range_scaled, y_range_scaled, zfactor
+        )
+    else:
+        raise TypeError('Unsupported Array Type: {}'.format(type(agg.data)))
 
-    data = (data - np.min(data))/np.ptp(data)
-    data[data < 0.3] = 0  # create water
-    data *= zfactor
+    canvas = ds.Canvas(
+        plot_width=width, plot_height=height, x_range=x_range, y_range=y_range
+    )
 
     # DataArray coords were coming back different from cvs.points...
     hack_agg = canvas.points(pd.DataFrame({'x': [], 'y': []}), 'x', 'y')
-    agg = DataArray(data,
-                    name='terrain',
-                    coords=hack_agg.coords,
-                    dims=hack_agg.dims,
-                    attrs={'res': 1})
+    result = xr.DataArray(out,
+                          name=name,
+                          coords=hack_agg.coords,
+                          dims=hack_agg.dims,
+                          attrs={'res': 1})
 
-    return agg
-
-
-def _gen_terrain(width, height, seed, x_range=None, y_range=None):
-
-    if not x_range:
-        x_range = (0, 1)
-
-    if not y_range:
-        y_range = (0, 1)
-
-    # multiplier, (xfreq, yfreq)
-    NOISE_LAYERS = ((1 / 2**i, (2**i, 2**i)) for i in range(16))
-
-    linx = np.linspace(x_range[0], x_range[1], width, endpoint=False)
-    liny = np.linspace(y_range[0], y_range[1], height, endpoint=False)
-    x, y = np.meshgrid(linx, liny)
-
-    height_map = None
-    for i, (m, (xfreq, yfreq)) in enumerate(NOISE_LAYERS):
-        noise = _perlin(x * xfreq, y * yfreq, seed=seed + i) * m
-        if height_map is None:
-            height_map = noise
-        else:
-            height_map += noise
-
-    height_map /= (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03)
-    height_map = height_map ** 3
-    return height_map
+    return result
