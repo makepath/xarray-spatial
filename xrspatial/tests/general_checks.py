8d93bc281a98d910e665e4e2e437af7e2b36e613
thuydotm <thuydtm.csk9@gmail.com>
2021-12-19 15:59:13 +0700

    added tests for output type, shape, coords and attrs

---
+++ xrspatial/tests/general_checks.py
@@ -0,0 +1,20 @@
+import numpy as np
+import dask.array as da
+
+
+def general_output_checks(input_agg, output_agg):
+
+    # type of output is the same as of input
+    assert isinstance(output_agg.data, type(input_agg.data))
+
+    if isinstance(input_agg.data, da.Array):
+        # dask case
+        assert isinstance(
+            output_agg.data.compute(), type(input_agg.data.compute()))
+
+    # shape and other attributes remain the same
+    assert output_agg.shape == input_agg.shape
+    assert output_agg.dims == input_agg.dims
+    assert output_agg.attrs == input_agg.attrs
+    for coord in input_agg.coords:
+        assert np.all(output_agg[coord] == input_agg[coord])
--- xrspatial/tests/test_classify.py
+++ xrspatial/tests/test_classify.py
@@ -5,11 +5,14 @@ import numpy as np
 
 import dask.array as da
 
-from xrspatial.utils import doesnt_have_cuda, is_cupy_backed
+from xrspatial.utils import doesnt_have_cuda
 from xrspatial import equal_interval
 from xrspatial import natural_breaks
 from xrspatial import quantile
 from xrspatial import reclassify
+
+from xrspatial.tests.general_checks import general_output_checks
+
 
 elevation = np.array([
     [1.,  2.,  3.,  4., np.nan],
@@ -24,25 +27,30 @@ dask_numpy_agg = xr.DataArray(da.from_array(elevation, chunks=(3, 3)),
 
 
 def test_reclassify_cpu():
-    bins = [10, 20]
-    new_values = [1, 2]
-
+    bins = [10, 15, np.inf]
+    new_values = [1, 2, 3]
+    expected_results = np.asarray([
+        [1., 1., 1., 1., np.nan],
+        [1., 1., 1., 1., 1.],
+        [1., 2., 2., 2., 2.],
+        [2., 3., 3., 3., 3.]
+    ])
     # numpy
-    numpy_reclassify = reclassify(numpy_agg, bins=bins, new_values=new_values,
-                                  name='numpy_reclassify')
-    # ignore nans
-    unique_elements = np.unique(
-        numpy_reclassify.data[np.isfinite(numpy_reclassify.data)]
+    numpy_reclassify = reclassify(
+        numpy_agg, bins=bins, new_values=new_values, name='numpy_reclassify'
     )
-    assert len(unique_elements) == 2
+    general_output_checks(numpy_agg, numpy_reclassify)
+    assert np.isclose(
+        numpy_reclassify.data, expected_results, equal_nan=True).all()
 
     # dask + numpy
-    dask_reclassify = reclassify(dask_numpy_agg, bins=bins,
-                                 new_values=new_values, name='dask_reclassify')
-    assert isinstance(dask_reclassify.data, da.Array)
-
-    dask_reclassify.data = dask_reclassify.data.compute()
-    assert np.isclose(numpy_reclassify, dask_reclassify, equal_nan=True).all()
+    dask_reclassify = reclassify(
+        dask_numpy_agg, bins=bins,
+        new_values=new_values, name='dask_reclassify'
+    )
+    general_output_checks(dask_numpy_agg, dask_reclassify)
+    assert np.isclose(
+        numpy_reclassify, dask_reclassify.compute(), equal_nan=True).all()
 
 
 @pytest.mark.skipif(doesnt_have_cuda(), reason="CUDA Device not Available")
@@ -50,7 +58,7 @@ def test_reclassify_cpu_equals_gpu():
 
     import cupy
 
-    bins = [10, 20, 30]
+    bins = [10, 15, np.inf]
     new_values = [1, 2, 3]
 
     # vanilla numpy version
@@ -66,35 +74,44 @@ def test_reclassify_cpu_equals_gpu():
                      name='cupy_result',
                      bins=bins,
                      new_values=new_values)
-    assert isinstance(gpu.data, cupy.ndarray)
+    general_output_checks(cupy_agg, gpu)
     assert np.isclose(cpu, gpu.data.get(), equal_nan=True).all()
 
     # dask + cupy
-    dask_cupy_agg = xr.DataArray(cupy.asarray(elevation),
-                                 attrs={'res': (10.0, 10.0)})
+    dask_cupy_agg = xr.DataArray(
+        cupy.asarray(elevation), attrs={'res': (10.0, 10.0)})
     dask_cupy_agg.data = da.from_array(dask_cupy_agg.data, chunks=(3, 3))
-    dask_gpu = reclassify(dask_cupy_agg, name='dask_cupy_result',
-                          bins=bins, new_values=new_values)
-    assert isinstance(dask_gpu.data, da.Array) and is_cupy_backed(dask_gpu)
-
+    dask_gpu = reclassify(
+        dask_cupy_agg, name='dask_cupy_result',
+        bins=bins, new_values=new_values
+    )
+    general_output_checks(dask_cupy_agg, dask_gpu)
     dask_gpu.data = dask_gpu.data.compute()
     assert np.isclose(cpu, dask_gpu.data.get(), equal_nan=True).all()
 
 
 def test_quantile_cpu():
     k = 5
-
+    expected_results = np.asarray([
+        [0., 0., 0., 0., np.nan],
+        [1., 1., 1., 2., 2.],
+        [2., 2., 3., 3., 3.],
+        [4., 4., 4., 4., np.nan]
+    ])
     # numpy
     numpy_quantile = quantile(numpy_agg, k=k)
-    unique_elements = np.unique(
-        numpy_quantile.data[np.isfinite(numpy_quantile.data)]
-    )
-    assert isinstance(numpy_quantile.data, np.ndarray)
-    assert len(unique_elements) == k
+    general_output_checks(numpy_agg, numpy_quantile)
+    assert np.isclose(
+        numpy_quantile.data, expected_results, equal_nan=True).all()
 
     # dask + numpy
     dask_quantile = quantile(dask_numpy_agg, k=k)
-    assert isinstance(dask_quantile.data, da.Array)
+    general_output_checks(dask_numpy_agg, dask_quantile)
+    dask_quantile = dask_quantile.compute()
+    unique_elements = np.unique(
+        dask_quantile.data[np.isfinite(dask_quantile.data)]
+    )
+    assert len(unique_elements) == k
 
     #     Note that dask's percentile algorithm is
     #     approximate, while numpy's is exact.
@@ -113,35 +130,29 @@ def test_quantile_cpu_equals_gpu():
     import cupy
 
     k = 5
-
     # vanilla numpy version
     cpu = quantile(numpy_agg, k=k, name='numpy_result')
-
     # cupy
-    cupy_agg = xr.DataArray(cupy.asarray(elevation),
-                            attrs={'res': (10.0, 10.0)})
+    cupy_agg = xr.DataArray(
+        cupy.asarray(elevation), attrs={'res': (10.0, 10.0)})
     gpu = quantile(cupy_agg, k=k, name='cupy_result')
-
-    assert isinstance(gpu.data, cupy.ndarray)
+    general_output_checks(cupy_agg, gpu)
     assert np.isclose(cpu, gpu.data.get(), equal_nan=True).all()
 
 
 def test_natural_breaks_cpu():
     k = 5
-
+    expected_results = np.asarray([
+        [0., 0., 0., 1., np.nan],
+        [1., 1., 2., 2., 2.],
+        [2., 3., 3., 3., 3.],
+        [4., 4., 4., 4., np.nan]
+    ])
     # vanilla numpy
     numpy_natural_breaks = natural_breaks(numpy_agg, k=k)
-    # shape and other attributes remain the same
-    assert numpy_agg.shape == numpy_natural_breaks.shape
-    assert numpy_agg.dims == numpy_natural_breaks.dims
-    assert numpy_agg.attrs == numpy_natural_breaks.attrs
-    for coord in numpy_agg.coords:
-        assert np.all(numpy_agg[coord] == numpy_natural_breaks[coord])
-
-    unique_elements = np.unique(
-        numpy_natural_breaks.data[np.isfinite(numpy_natural_breaks.data)]
-    )
-    assert len(unique_elements) == k
+    general_output_checks(numpy_agg, numpy_natural_breaks)
+    assert np.isclose(
+        numpy_natural_breaks.data, expected_results, equal_nan=True).all()
 
 
 def test_natural_breaks_cpu_deterministic():
@@ -154,13 +165,7 @@ def test_natural_breaks_cpu_deterministic():
     for i in range(numIters):
         # vanilla numpy
         numpy_natural_breaks = natural_breaks(agg, k=k)
-        # shape and other attributes remain the same
-        assert agg.shape == numpy_natural_breaks.shape
-        assert agg.dims == numpy_natural_breaks.dims
-        assert agg.attrs == numpy_natural_breaks.attrs
-        for coord in agg.coords:
-            assert np.all(agg[coord] == numpy_natural_breaks[coord])
-
+        general_output_checks(agg, numpy_natural_breaks)
         unique_elements = np.unique(
             numpy_natural_breaks.data[np.isfinite(numpy_natural_breaks.data)]
         )
@@ -178,33 +183,34 @@ def test_natural_breaks_cpu_equals_gpu():
     import cupy
 
     k = 5
-
     # vanilla numpy version
     cpu = natural_breaks(numpy_agg, k=k, name='numpy_result')
-
     # cupy
     cupy_agg = xr.DataArray(cupy.asarray(elevation),
                             attrs={'res': (10.0, 10.0)})
     gpu = natural_breaks(cupy_agg, k=k, name='cupy_result')
-
-    assert isinstance(gpu.data, cupy.ndarray)
+    general_output_checks(cupy_agg, gpu)
     assert np.isclose(cpu, gpu.data.get(), equal_nan=True).all()
 
 
 def test_equal_interval_cpu():
-    k = 5
-    # numpy
-    numpy_ei = equal_interval(numpy_agg, k=5)
+    k = 3
+    expected_results = np.asarray([
+        [0., 0., 0., 0., np.nan],
+        [0., 0., 1., 1., 1.],
+        [1., 1., 1., 2., 2.],
+        [2., 2., 2., 2., np.nan]
+    ])
 
-    unique_elements = np.unique(numpy_ei.data[da.isfinite(numpy_ei.data)])
-    assert isinstance(numpy_ei.data, np.ndarray)
-    assert len(unique_elements) == k
+    # numpy
+    numpy_ei = equal_interval(numpy_agg, k=k)
+    general_output_checks(numpy_agg, numpy_ei)
+    assert np.isclose(numpy_ei.data, expected_results, equal_nan=True).all()
 
     # dask + numpy
     dask_ei = equal_interval(dask_numpy_agg, k=k, name='dask_reclassify')
-    assert isinstance(dask_ei.data, da.Array)
-
-    dask_ei.data = dask_ei.data.compute()
+    general_output_checks(dask_numpy_agg, dask_ei)
+    dask_ei = dask_ei.compute()
     assert np.isclose(numpy_ei, dask_ei, equal_nan=True).all()
 
 
@@ -213,15 +219,12 @@ def test_equal_interval_cpu_equals_gpu():
 
     import cupy
 
-    k = 5
-
+    k = 3
     # numpy
     cpu = equal_interval(numpy_agg, k=k)
-
     # cupy
-    cupy_agg = xr.DataArray(cupy.asarray(elevation),
-                            attrs={'res': (10.0, 10.0)})
+    cupy_agg = xr.DataArray(
+        cupy.asarray(elevation), attrs={'res': (10.0, 10.0)})
     gpu = equal_interval(cupy_agg, k=k)
-    assert isinstance(gpu.data, cupy.ndarray)
-
+    general_output_checks(cupy_agg, gpu)
     assert np.isclose(cpu, gpu.data.get(), equal_nan=True).all()
