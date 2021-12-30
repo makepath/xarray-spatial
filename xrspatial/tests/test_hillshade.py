d906bbb4e6b095ebae051fabf4b06a3ef9d8f39b
thuydotm <thuydtm.csk9@gmail.com>
2021-12-30 14:53:00 +0700

    test hillshade

--- xrspatial/tests/test_hillshade.py
+++ xrspatial/tests/test_hillshade.py
@@ -7,6 +7,8 @@ import dask.array as da
 from xrspatial import hillshade
 from xrspatial.utils import doesnt_have_cuda
 
+from xrspatial.tests.general_checks import general_output_checks
+
 
 elevation = np.asarray(
     [[1432.6542, 1432.4764, 1432.4764, 1432.1207, 1431.9429, np.nan],
@@ -41,8 +43,6 @@ def _do_gaussian_array():
     y_fac = -np.power(Y-_mean, 2)
     gaussian = np.exp((x_fac+y_fac)/(2*_sdev**2)) / (2.5*_sdev)
     return gaussian
-#
-# -----
 
 
 data_random = np.random.random_sample((100, 100))
@@ -56,27 +56,24 @@ def test_hillshade():
     """
     da_gaussian = xr.DataArray(data_gaussian)
     da_gaussian_shade = hillshade(da_gaussian, name='hillshade_agg')
-    assert da_gaussian_shade.dims == da_gaussian.dims
-    assert da_gaussian_shade.attrs == da_gaussian.attrs
+    general_output_checks(da_gaussian, da_gaussian_shade)
     assert da_gaussian_shade.name == 'hillshade_agg'
-    for coord in da_gaussian.coords:
-        assert np.all(da_gaussian_shade[coord] == da_gaussian[coord])
     assert da_gaussian_shade.mean() > 0
     assert da_gaussian_shade[60, 60] > 0
 
 
-def test_numpy_equals_dask():
+def test_hillshade_cpu():
     attrs = {'res': (10.0, 10.0)}
 
     small_numpy_based_data_array = xr.DataArray(elevation, attrs=attrs)
+    numpy_result = hillshade(small_numpy_based_data_array, name='numpy')
+
     dask_data = da.from_array(elevation, chunks=(3, 3))
     small_das_based_data_array = xr.DataArray(dask_data, attrs=attrs)
-
-    numpy_result = hillshade(small_numpy_based_data_array, name='numpy')
     dask_result = hillshade(small_das_based_data_array, name='dask')
-    dask_result.data = dask_result.data.compute()
 
-    assert np.isclose(numpy_result, dask_result, equal_nan=True).all()
+    general_output_checks(small_das_based_data_array, dask_result)
+    np.testing.assert_allclose(numpy_result, dask_result, equal_nan=True)
 
 
 @pytest.mark.skipif(doesnt_have_cuda(), reason="CUDA Device not Available")
@@ -91,6 +88,5 @@ def test_hillshade_gpu_equals_cpu():
                                  attrs={'res': (10.0, 10.0)})
     gpu = hillshade(small_da_cupy, name='cupy_result')
 
-    assert isinstance(gpu.data, cupy.ndarray)
-
-    assert np.isclose(cpu, gpu.data.get(), equal_nan=True).all()
+    general_output_checks(small_da_cupy, gpu)
+    assert np.isclose(cpu.data, gpu.data.get(), equal_nan=True).all()
