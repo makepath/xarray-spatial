3402d564bc55f9a6e26efe353728f0d13c09df60
Ian Thomas <ianthomas23@gmail.com>
2022-01-07 17:09:07 +0000

    Add hillshade benchmarking, for numpy, cupy and rtxpy (#625)

---
+++ benchmarks/benchmarks/hillshade.py
@@ -0,0 +1,18 @@
+from xrspatial import hillshade
+from .common import get_xr_dataarray
+
+
+class Hillshade:
+    # Note that rtxpy hillshade includes shadow calculations so timings are
+    # not comparable with numpy and cupy hillshade.
+    params = ([100, 300, 1000, 3000], ["numpy", "cupy", "rtxpy"])
+    param_names = ("nx", "type")
+
+    def setup(self, nx, type):
+        ny = nx // 2
+        self.xr = get_xr_dataarray(
+            (ny, nx), type, different_each_call=(type == "rtxpy"))
+
+    def time_hillshade(self, nx, type):
+        shadows = (type == "rtxpy")
+        hillshade(self.xr, shadows=shadows)
