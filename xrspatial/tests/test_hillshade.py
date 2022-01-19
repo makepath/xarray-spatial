75fb63223fa89c8eb506a3bbfe1de897c64f86d9
thuydotm <thuydtm.csk9@gmail.com>
2022-01-19 13:09:36 +0700

    test hillshade

--- xrspatial/tests/test_hillshade.py
+++ xrspatial/tests/test_hillshade.py
@@ -3,39 +3,18 @@ import xarray as xr
 import numpy as np
 from numpy.testing import assert_allclose, assert_array_less
 
-import dask.array as da
-
 from xrspatial import hillshade
 from xrspatial.utils import doesnt_have_cuda
 from ..gpu_rtx import has_rtx
 
 from xrspatial.tests.general_checks import general_output_checks
+from xrspatial.tests.general_checks import create_test_raster
+from xrspatial.tests.general_checks import assert_numpy_equals_dask_numpy
+from xrspatial.tests.general_checks import assert_numpy_equals_cupy
 
 
-elevation = np.asarray(
-    [[1432.6542, 1432.4764, 1432.4764, 1432.1207, 1431.9429, np.nan],
-     [1432.6542, 1432.6542, 1432.4764, 1432.2986, 1432.1207, np.nan],
-     [1432.832, 1432.6542, 1432.4764, 1432.2986, 1432.1207, np.nan],
-     [1432.832, 1432.6542, 1432.4764, 1432.4764, 1432.1207, np.nan],
-     [1432.832, 1432.6542, 1432.6542, 1432.4764, 1432.2986, np.nan],
-     [1432.832, 1432.6542, 1432.6542, 1432.4764, 1432.2986, np.nan],
-     [1432.832, 1432.832, 1432.6542, 1432.4764, 1432.4764, np.nan]],
-    dtype=np.float32)
-
-
-def _do_sparse_array(data_array):
-    import random
-    indx = list(zip(*np.where(data_array)))
-    pos = random.sample(range(data_array.size), data_array.size//2)
-    indx = np.asarray(indx)[pos]
-    r = indx[:, 0]
-    c = indx[:, 1]
-    data_half = data_array.copy()
-    data_half[r, c] = 0
-    return data_half
-
-
-def _do_gaussian_array():
+@pytest.fixture
+def data_gaussian():
     _x = np.linspace(0, 50, 101)
     _y = _x.copy()
     _mean = 25
@@ -47,12 +26,7 @@ def _do_gaussian_array():
     return gaussian
 
 
-data_random = np.random.random_sample((100, 100))
-data_random_sparse = _do_sparse_array(data_random)
-data_gaussian = _do_gaussian_array()
-
-
-def test_hillshade():
+def test_hillshade(data_gaussian):
     """
     Assert Simple Hillshade transfer function
     """
@@ -64,37 +38,23 @@ def test_hillshade():
     assert da_gaussian_shade[60, 60] > 0
 
 
-def test_hillshade_cpu():
-    attrs = {'res': (10.0, 10.0)}
-
-    small_numpy_based_data_array = xr.DataArray(elevation, attrs=attrs)
-    numpy_result = hillshade(small_numpy_based_data_array, name='numpy')
-
-    dask_data = da.from_array(elevation, chunks=(3, 3))
-    small_das_based_data_array = xr.DataArray(dask_data, attrs=attrs)
-    dask_result = hillshade(small_das_based_data_array, name='dask')
-
-    general_output_checks(small_das_based_data_array, dask_result)
-    np.testing.assert_allclose(
-        numpy_result.data, dask_result.data.compute(), equal_nan=True)
+@pytest.mark.parametrize("size", [(2, 4), (10, 15)])
+@pytest.mark.parametrize(
+    "dtype", [np.int32, np.int64, np.float32, np.float64])
+def test_hillshade_numpy_equals_dask_numpy(random_data):
+    numpy_agg = create_test_raster(random_data, backend='numpy')
+    dask_agg = create_test_raster(random_data, backend='dask')
+    assert_numpy_equals_dask_numpy(numpy_agg, dask_agg, hillshade)
 
 
 @pytest.mark.skipif(doesnt_have_cuda(), reason="CUDA Device not Available")
-def test_hillshade_gpu_equals_cpu():
-
-    import cupy
-
-    small_da = xr.DataArray(elevation, attrs={'res': (10.0, 10.0)})
-    cpu = hillshade(small_da, name='numpy_result')
-
-    small_da_cupy = xr.DataArray(cupy.asarray(elevation),
-                                 attrs={'res': (10.0, 10.0)})
-    gpu = hillshade(small_da_cupy, name='cupy_result')
-
-    general_output_checks(small_da_cupy, gpu)
-    assert isinstance(gpu.data, cupy.ndarray)
-
-    assert np.isclose(cpu.data, gpu.data.get(), equal_nan=True).all()
+@pytest.mark.parametrize("size", [(2, 4), (10, 15)])
+@pytest.mark.parametrize(
+    "dtype", [np.int32, np.int64, np.float32, np.float64])
+def test_hillshade_gpu_equals_cpu(random_data):
+    numpy_agg = create_test_raster(random_data, backend='numpy')
+    cupy_agg = create_test_raster(random_data, backend='cupy')
+    assert_numpy_equals_cupy(numpy_agg, cupy_agg, hillshade)
 
 
 @pytest.mark.skipif(not has_rtx(), reason="RTX not available")
