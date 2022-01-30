fa1dff3a36552b89eb9420958854aa9d321dd5c2
thuydotm <thuydtm.csk9@gmail.com>
2022-01-30 21:13:24 +0700

    renamed  parameter to

--- xrspatial/analytics.py
+++ xrspatial/analytics.py
@@ -5,32 +5,32 @@ from xrspatial import aspect
 import xarray as xr
 
 
-def summarize_terrain(raster: xr.DataArray):
+def summarize_terrain(terrain: xr.DataArray):
     """
-    Calculates slope, aspect, and curvature of an elevation raster and return a dataset
+    Calculates slope, aspect, and curvature of an elevation terrain and return a dataset
     of the computed data.
 
     Parameters
     ----------
-    raster: xarray.DataArray
+    terrain: xarray.DataArray
         2D NumPy, CuPy, or Dask with NumPy-backed xarray DataArray of elevation values.
 
     Returns
     -------
     summarized_terrain: xarray.Dataset
         Dataset with slope, aspect, curvature variables with a naming convention of
-        `raster.name-variable_name`
+        `terrain.name-variable_name`
 
     Examples
     --------
 
     """
 
-    if raster.name is None:
+    if terrain.name is None:
         raise NameError('Requires xr.DataArray.name property to be set')
 
-    ds = raster.to_dataset()
-    ds[f'{raster.name}-slope'] = slope(raster)
-    ds[f'{raster.name}-curvature'] = curvature(raster)
-    ds[f'{raster.name}-aspect'] = aspect(raster)
+    ds = terrain.to_dataset()
+    ds[f'{terrain.name}-slope'] = slope(terrain)
+    ds[f'{terrain.name}-curvature'] = curvature(terrain)
+    ds[f'{terrain.name}-aspect'] = aspect(terrain)
     return ds
