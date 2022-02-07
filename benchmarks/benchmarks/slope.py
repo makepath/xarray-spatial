7e432d3f396b3fe48a81ec73cbe6dd46d0380b74
thuydotm <thuydtm.csk9@gmail.com>
2022-02-07 23:12:29 +0700

    slope

--- benchmarks/benchmarks/slope.py
+++ benchmarks/benchmarks/slope.py
@@ -1,14 +1,10 @@
 from xrspatial import slope
-from .common import get_xr_dataarray
+from .common import Benchmarking
 
 
-class Slope:
-    params = ([100, 300, 1000, 3000, 10000], ["numpy", "cupy"])
-    param_names = ("nx", "type")
-
-    def setup(self, nx, type):
-        ny = nx // 2
-        self.xr = get_xr_dataarray((ny, nx), type)
+class Slope(Benchmarking):
+    def __init__(self):
+        super().__init__(func=slope)
 
     def time_slope(self, nx, type):
-        slope(self.xr)
+        return self.time(nx, type)
