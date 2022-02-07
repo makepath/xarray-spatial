5d5017de589ba1bdf5d756c76db7cd66faac4342
thuydotm <thuydtm.csk9@gmail.com>
2022-02-07 17:03:52 +0700

    curvature

---
+++ benchmarks/benchmarks/curvature.py
@@ -0,0 +1,10 @@
+from xrspatial import curvature
+from .common import Benchmarking
+
+
+class Curvature(Benchmarking):
+    def __init__(self):
+        super().__init__(func=curvature)
+
+    def time_curvature(self, nx, type):
+        return self.time(nx, type)
