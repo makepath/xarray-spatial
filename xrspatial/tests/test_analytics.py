8499a69e037eae13451b159806d544e8086400f8
thuydotm <thuydtm.csk9@gmail.com>
2022-02-07 12:07:18 +0700

    test analytics: use random_data pytest fixture

--- xrspatial/tests/test_analytics.py
+++ xrspatial/tests/test_analytics.py
@@ -1,36 +1,18 @@
 import pytest
 import numpy as np
-import xarray as xr
 
 from xrspatial import aspect
 from xrspatial import curvature
 from xrspatial import slope
 from xrspatial.analytics import summarize_terrain
 
-
-@pytest.fixture
-def test_terrain():
-    # TODO: use function random_data() from global pytest fixture in xrspatial.tests.conftest
-    #  once it gets merged
-    data = np.array([
-        [10., 10., 10., 10.],
-        [0., 0., 0., 0.],
-        [10., 10., 10., 10.],
-        [20., 20., 20., 20.],
-        [20., 20., np.nan, 20.],
-        [30., 30., 30., 30.]
-    ])
-    terrain = xr.DataArray(
-        data,
-        dims=['y', 'x'],
-        coords={'y': np.arange(0, 6),
-                'x': np.arange(0, 4)},
-        name='myterrain'
-    )
-    return terrain
+from xrspatial.tests.general_checks import create_test_raster
 
 
-def test_summarize_terrain(test_terrain):
+@pytest.mark.parametrize("size", [(2, 4), (100, 150)])
+@pytest.mark.parametrize("dtype", [np.int32, np.int64, np.uint32, np.uint64, np.float32, np.float64])
+def test_summarize_terrain(random_data):
+    test_terrain = create_test_raster(random_data, name='myterrain')
     summarized_ds = summarize_terrain(test_terrain)
     variables = [v for v in summarized_ds]
     should_have = ['myterrain',
@@ -38,6 +20,7 @@ def test_summarize_terrain(test_terrain):
                    'myterrain-curvature',
                    'myterrain-aspect']
     assert variables == should_have
+
     np.testing.assert_allclose(summarized_ds['myterrain-slope'], slope(test_terrain))
     np.testing.assert_allclose(summarized_ds['myterrain-curvature'], curvature(test_terrain))
     np.testing.assert_allclose(summarized_ds['myterrain-aspect'], aspect(test_terrain))
