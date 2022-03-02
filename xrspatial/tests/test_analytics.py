50e17530485d021fecf0d30c79946c9590a37c75
thuydotm <thuydtm.csk9@gmail.com>
2022-03-02 11:14:23 +0700

    analytics: test no name

--- xrspatial/tests/test_analytics.py
+++ xrspatial/tests/test_analytics.py
@@ -6,6 +6,14 @@ from xrspatial.analytics import summarize_terrain
 from xrspatial.tests.general_checks import create_test_raster
 
 
+def test_summarize_terrain_no_name():
+    data = np.zeros((10, 20))
+    test_terrain = create_test_raster(data, name=None)
+    msg = "Requires xr.DataArray.name property to be set"
+    with pytest.raises(NameError, match=msg):
+        summarize_terrain(test_terrain)
+
+
 @pytest.mark.parametrize("size", [(2, 4), (100, 150)])
 @pytest.mark.parametrize(
     "dtype", [np.int32, np.int64, np.uint32, np.uint64, np.float32, np.float64]
