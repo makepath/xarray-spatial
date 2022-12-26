5ac505378ab091d083c276045b49e0f39e96b681
thuydotm <thuydtm.csk9@gmail.com>
2022-12-26 15:37:54 +0700

    data for testing proximity distance

--- xrspatial/tests/conftest.py
+++ xrspatial/tests/conftest.py
@@ -24,3 +24,17 @@ def elevation_raster():
     ], dtype=np.float32)
     return elevation
 
+
+@pytest.fixture
+def raster():
+    data = np.array([
+        [4., 2., 2., 3., 6., 1.],
+        [9., 4., 2., 4., 9., 7.],
+        [2., 7., 0., 6., 7., 9.],
+        [3., 0., 9., 7., 4., 4.],
+        [7., 7., 1., 5., 4., 6.],
+        [1., 3., 4., 1., 7., 7.],
+        [7., 8., 4., 0., 1., 0.],
+        [0., 3., 5., 8., 9., 0.]
+    ], dtype=np.float32)
+    return data
