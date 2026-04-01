f301f28e5a26231dc4fbab655afa6e1ac85a5a0f
Brendan Collins <brendancol@gmail.com>
2026-04-01 11:31:32 -0700

    Add empty visibility module scaffold (#1145)

---
+++ xrspatial/visibility.py
@@ -0,0 +1,12 @@
+"""
+Multi-observer viewshed and line-of-sight profile tools.
+
+Functions
+---------
+cumulative_viewshed
+    Count how many observers can see each cell.
+visibility_frequency
+    Fraction of observers that can see each cell.
+line_of_sight
+    Elevation profile and visibility along a straight line between two points.
+"""
