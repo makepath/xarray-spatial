62816518624fe04cdd73e77c9128ad7d545d8e37
Brendan Collins <brendancol@gmail.com>
2026-06-25 11:32:57 -0700

    Document num_sample=None in natural_breaks docstring (#3501)
    natural_breaks accepts num_sample=None (use all data) like the other
    classifiers, but its docstring only described it as int. Bring the
    parameter wording in line with quantile/maximum_breaks/percentiles.

--- xrspatial/classify.py
+++ xrspatial/classify.py
@@ -842,12 +842,14 @@ def natural_breaks(agg: xr.DataArray,
         of values to be reclassified.
     k : int, default=5
         Number of classes to be produced.
-    num_sample : int, default=20000
+    num_sample : int or None, default=20000
         Number of sample data points used to fit the model.
         Natural Breaks (Jenks) classification is indeed O(n²) complexity,
         where n is the total number of data points, i.e: `agg.size`
         When n is large, we should fit the model on a small sub-sample
         of the data instead of using the whole dataset.
+        ``None`` means use all data (safe for numpy/cupy,
+        automatically capped for dask).
     name : str, default='natural_breaks'
         Name of output aggregate.
 
